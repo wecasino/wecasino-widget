5441395d7b76b4dab4e7305d3402dab36377bb70
pcxyz <120380827+peterchanxyz@users.noreply.github.com>
2024-07-24 17:08:50 +0800

    feat: vite buid js

---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+import pkg from "./package.json";
+
+// https://vitejs.dev/guide/build.html#library-mode
+export default defineConfig({
+  build: {
+    target: ["es2020", "edge79", "firefox67", "chrome64", "safari12"],
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "gdsapi-ts-lib",
+      fileName: `wecasino-widget-${pkg.version}`,
+      formats: ["es", "cjs"],
+    },
+    rollupOptions: {
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          WeClient: "WeClient",
+          WeCompoent: "WeCompoent",
+        },
+      },
+    },
+  },
+  plugins: [dts()],
+});
