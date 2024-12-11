d6c5a3cb4e450b16a73975ea165f1f5ee32143bd
Leo Ng <leo.ng.dev@pgpg.la>
2024-12-11 17:38:38 +0800

    fix: missing update

--- packages/wewidget/src/react/hooks/useWeGameWidget.tsx
+++ packages/wewidget/src/react/hooks/useWeGameWidget.tsx
@@ -1,7 +1,42 @@
 import { useStore } from "zustand";
-import { calcBAStats } from "@wecasino/weroadmap";
+import { calcBAStats, calcCGStats } from "@wecasino/weroadmap";
+import { type CGStats } from "@wecasino/weroadmap";
 import { weClientInstance } from "../../core";
+import { useMemo } from "react";
 
+const toPercentages = (ns: number[] = [], upto = 100): number[] => {
+  if (ns.length === 0) return [];
+
+  // inputs sum to 100
+  const sum = ns.reduce((sum, item) => sum + item, 0);
+  const pp = ns.map((p) => (p / sum) * upto);
+
+  // adjust
+  const ap = pp.map((p) => ({
+    value: p,
+    floor: Math.floor(p),
+    decimal: p % 1,
+  }));
+
+  // sort and add remains to larger value
+  const remains = upto - ap.reduce((sum, p) => sum + p.floor, 0);
+  ap.sort((a, b) => {
+    // 0 < x < 1 -> 1
+    if (a.value > 0 && a.value < 1) return -1;
+    return b.decimal - a.decimal;
+  });
+  for (let i = 0; i < remains; i++) {
+    ap[i]!.floor += 1;
+  }
+
+  // re sort to original
+  const ret = pp.map((p) => {
+    const item = ap.find((i) => i.value === p);
+    return item ? item.floor : 0;
+  });
+
+  return ret;
+};
 const RoundState = {
   SHUFFLE: "shuffle",
   NEW_SHOE: "newshoe",
@@ -10,6 +45,41 @@ const RoundState = {
   CARD: "card",
   RESULT: "result",
   CANCEL: "cancel",
+};
+
+export const BetCodeCG = {
+  PINK: "PINK",
+  GREEN: "GREEN",
+  YELLOW: "YELLOW",
+  RED: "RED",
+  WHITE: "WHITE",
+  BLUE: "BLUE",
+};
+
+export const betCodeCGTypes = [
+  BetCodeCG.PINK,
+  BetCodeCG.GREEN,
+  BetCodeCG.YELLOW,
+  BetCodeCG.RED,
+  BetCodeCG.WHITE,
+  BetCodeCG.BLUE,
+];
+
+const CGBetCodeColorsInTitlecase = {
+  [BetCodeCG.YELLOW]: "Yellow",
+  [BetCodeCG.WHITE]: "White",
+  [BetCodeCG.PINK]: "Pink",
+  [BetCodeCG.BLUE]: "Blue",
+  [BetCodeCG.RED]: "Red",
+  [BetCodeCG.GREEN]: "Green",
+} as const;
+
+type ValueOf<T> = T[keyof T];
+
+const convertedBetCode = (betCode: string) => {
+  return CGBetCodeColorsInTitlecase[betCode] as ValueOf<
+    typeof CGBetCodeColorsInTitlecase
+  >;
 };
 
 const getIsChangingShoe = (roundState: string) => {
@@ -69,6 +139,20 @@ export default ({ gameCode }: { gameCode: string }) => {
   const isMaintenance = gameState === "maintenance" || gameState === "disable";
 
   const gameType = game?.gameInfo?.gameType;
+
+  const cgStatsPercentage = useMemo(() => {
+    if (["CG", "CGM"].indexOf(gameType || "") === -1) return [];
+    const sliced = accumCards.slice(-999);
+    const stats = calcCGStats(sliced);
+    return toPercentages(
+      betCodeCGTypes.map((betCode) => {
+        const betCount =
+          stats[`cnt${convertedBetCode(betCode)}` as keyof CGStats] ?? 0;
+        return betCount;
+      })
+    );
+  }, [gameType, accumCards]);
+
   return {
     game,
     gameType,
@@ -85,5 +169,6 @@ export default ({ gameCode }: { gameCode: string }) => {
     isMaintenance,
     coverImageUrl,
     gameStateFlag,
+    cgStatsPercentage,
   };
 };
